eeaf490705e71f3d49f216e5bfa16f887b8f9296
hdlAudio <florian.mrugalla@web.de>
2020-05-17 19:19:26 +0200

    added autogain

--- 2020_05_14_testtoneGenerator/main.cxx
+++ 2020_05_14_testtoneGenerator/main.cxx
@@ -6,17 +6,19 @@ a little test tone generator. i made this so i can test the behaviour of
 fft algorithms but you can use it for whatever you want.
 */
 
-array<string> inputParametersNames = {"osc freq", "osc0", "osc1", "osc2", "osc3", "osc4", "osc5", "osc6", "osc7", "masterGain"};
+array<string> inputParametersNames = {"autogain", "osc freq", "osc0", "osc1", "osc2", "osc3", "osc4", "osc5", "osc6", "osc7", "masterGain"};
 array<string> inputParametersUnits = {};
 array<string> inputParametersEnums = {};
 array<double> inputParameters( inputParametersNames.length );
-array<int> inputParametersSteps = { -1, -1 };
-array<double> inputParametersDefault = { 50.f, 0.f };
-array<double> inputParametersMin = { 0.f, 0.f };
-array<double> inputParametersMax = { float(sampleRate * .5), 1.f };
+array<int> inputParametersSteps = { 2, -1 };
+array<double> inputParametersDefault = { 0, 50.f };
+array<double> inputParametersMin = { 0, 0.f };
+array<double> inputParametersMax = { 1, sampleRate * .5 };
 
 array<float>oscGain(8);
 float masterGain;
+int autogain, agIdx, agSize = int(sampleRate / 16.);
+float agMax, agValue = 1.f;
 
 Osc osc;
 
@@ -29,6 +31,9 @@ void processBlock(BlockData& data){
 		double wave = 0.;
 		for(uint h = 0; h < 8; ++h)
 			wave += osc.get(h) * oscGain[h];
+		
+		processAutogain(wave);
+		
 		wave *= masterGain;
 		
 		for(uint ch = 0; ch < audioInputsCount; ch++)
@@ -36,14 +41,32 @@ void processBlock(BlockData& data){
 	}
 }
 
+void processAutogain(double& wave){
+	if(autogain == 1){
+		if(agMax < wave)
+		agMax = wave;
+		++agIdx;
+		if(!(agIdx < agSize)){
+			agIdx = 0;
+			agValue = 1.f / agMax;
+			agMax = 0.f;
+		}
+		wave *= agValue;
+	}
+}
+
 void updateInputParameters(){
 	uint i = 0;
+	autogain = rint(inputParameters[0]);
+	++i;
 	osc.setFreq(inputParameters[i]);
 	++i;
-	while(i <= 8){
-		oscGain[i - 1] = inputParameters[i];
-		++i;
+	uint idx;
+	for(uint h = 0; h < 8; ++h){
+		idx = i + h;
+		oscGain[h] = inputParameters[idx];
 	}
+	i = idx + 1;
 	masterGain = inputParameters[i];
 }
 
@@ -55,11 +78,11 @@ int rint(double d){
 }
 
 class Osc{
-	double sampleRateInv = 2. / sampleRate, pi = 3.14159265359;
-	double phase = 0., inc = 0., piTwo = pi * 2.;
+	double pi = 3.14159265359, piTwo = pi * 2., sampleRatePiInv = piTwo / sampleRate;
+	double phase = 0., inc = 0.;
 	array<double> harmonics(8);
 	
-	void setFreq(double freq){ inc = pi * sampleRateInv * freq; }
+	void setFreq(double freq){ inc = sampleRatePiInv * freq; }
 	
 	void process(){
 		phase += inc;
